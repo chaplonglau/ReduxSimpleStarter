7b5bfc5a7d99680a2618c2b4682cacb4c400c240
chap <chaplonglau@gmail.com>
2016-08-16 15:50:33 -0400

    able to get basic videos showing up

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBLPTzL-yYZqChQpdi6x_956bWD9yGK8AM';
-
-
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state={videos: [] };
+    this.state={
+      videos: [],
+      selectedVideo: null
+   };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({videos});
@@ -25,7 +27,9 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
+
       </div>
     )
   }
