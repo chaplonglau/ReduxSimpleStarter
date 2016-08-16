21ef3c3a97d0c7170b6d1cffddcfd8797334b294
chap <chaplonglau@gmail.com>
2016-08-16 14:35:59 -0400

    searchbar with outputing text

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 //JSX is a dialect of Javascript
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBLPTzL-yYZqChQpdi6x_956bWD9yGK8AM';
 
 const App = () => {
-  return <div> HIiii! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('container'))
+ReactDOM.render(<App />, document.querySelector('.container'))
